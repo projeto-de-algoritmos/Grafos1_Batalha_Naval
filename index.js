54e17dbcce4c4202c80ce52bbe63311ef9151a0f
MariaAbritta <eduardaabritta@gmail.com>
2023-09-19 10:38:37 -0300

    json + gitgnore

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import PaginaInicial from './paginaInicial/paginaInicial';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PaginaInicial/>
+  </React.StrictMode>
+);
