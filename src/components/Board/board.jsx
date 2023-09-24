039f52fb0a7602c1194fee4d10c8f8c133dd0397
luciano-freitas-melo <lucianodefreitasmelo@hotmail.com>
2023-09-24 18:34:47 -0300

    anima navios e botoes do board

--- src/components/Board/board.jsx
+++ src/components/Board/board.jsx
@@ -1,8 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import icone_onda from "../../assets/ondas.png"
+import icone_navio from "../../assets/navio.png"
 import "./board.css"
 
 export function Board({ tabuleiro, dono }) {
+
+  const [toggleRender, setToggleRender] = useState(false);
+
+  function verificaItem(x_index, y_index) {
+    switch (tabuleiro[y_index][x_index]) {
+      case 0:
+        tabuleiro[y_index][x_index] = -1
+        setToggleRender(!toggleRender)
+        break
+      case 1:
+        tabuleiro[y_index][x_index] = 2
+        setToggleRender(!toggleRender)
+        break
+      default:
+        console.log(tabuleiro[y_index][x_index])
+        break
+    }
+  }
+
   return (
       // map está sendo usado para percorrer cada linha (line) e seu índice (y_index) no tabuleiro
       <div className="board">
@@ -15,12 +35,19 @@ export function Board({ tabuleiro, dono }) {
                 return(
                   <input
                     type="image"
-                    src={icone_onda}
+                    src={item == 2? icone_navio : icone_onda}
+
                     key={y_index + "" + x_index}
-                    className="board__item"
+                    alt="item"
+                    // o segundo class depende do valor do item
+                    // 0 ou 1 = ainda não foi visitado
+                    // 2 = visitado e é um navio
+                    // -1 = visitado e não é um navio
+                    className={`board__item board__item--${item}`}
+                    
+                    onClick={() => verificaItem(x_index, y_index)}
                     />
                     ) 
-                    
                   })}
           </div>
       ))}
